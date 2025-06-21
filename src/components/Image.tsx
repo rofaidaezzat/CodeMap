9ac45e96f42f3d28b0c0bd3e15b7bf0683e941c0
rofaidaezzat <rofaidaezzat@gmail.com>
2025-06-21 05:12:55 +0300

    edit6

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -7,8 +7,11 @@ interface Iprops extends ImgHTMLAttributes<HTMLImageElement>{
 }
 
 const Image = ({imageurl,alt,className,...rest}:Iprops) => {
+    const prodUrl = "https://codemapuser.netlify.app";
+    const isProduction = process.env.NODE_ENV === "production";
+    const src = isProduction ? `${prodUrl}${imageurl}` : imageurl;
     return (
-        <img src={imageurl} alt={alt} className={className} {...rest}/>
+        <img src={src} alt={alt} className={className} {...rest}/>
 )
 }
 
